e9ffdaff829b5a5ee9ec72b503f81b9d780e07f3
Camila Azevedo <camila.santiago026@gmail.com>
2023-04-06 17:54:03 -0300

    TutorialNode 2/7

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    database: 'devo-login'
+})
+
+db.connect( (error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('Mysql conectado!')
+    }
+});
 
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>")
