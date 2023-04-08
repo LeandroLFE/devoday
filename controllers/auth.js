be85e074988041d120e8113eb9bde3a456ac0e13
Camila Azevedo <camila.santiago026@gmail.com>
2023-04-08 17:51:31 -0300

    Início registro

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,26 @@
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
 exports.register = (req, res) => {
     console.log(req.body)
-    res.send("formulário enviado!")
+
+    const { email, password } = req.body;
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], (error, results) => {
+        if (error) {
+            console.log(error)
+        }
+
+        if (results.length > 0) {
+            return res.render('login', {
+                message: 'Este email já está em uso.'
+            })
+        } 
+    })
 }
